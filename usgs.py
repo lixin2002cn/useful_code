f514bf2ee5c797e76187f9e38a9c12c87e4b3401
Xin Li <167651328+lixin2002cn@users.noreply.github.com>
2024-05-12 23:42:34 +0800

    upload usgs.py
     a file to draw usgs spectral curve in usgs spectral library

---
+++ usgs.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+# 读取txt文件,从第二行开始读，读取的每行数据转成float
+    path = r"H:\biaoshu\ChapterS_SoilsAndMixtures\splib07a_Sand_DWO-3-DEL2d_wet_no_oil_ASDFRa_AREF.txt"
+    with open(path, 'r', encoding='UTF-8') as f:
+        lines = f.readlines()    # 接收数据
+        # print(len(lines))
+        head = lines[0]
+        head_list = head.split()
+        print(head_list)
+        float_spectal = []
+        value_to_remove = -1.23e+034
+        new_list = list(filter(lambda x: float(x) != value_to_remove, lines[1:]))
+        print(new_list)
+        for line in new_list:     # 遍历数据
+            float_spectal.append(float(line))
+        min_value = np.min(float_spectal)
+        max_value = np.max(float_spectal)
+        print(min_value, max_value)
+        normal_spectal = (np.array(float_spectal) - min_value ) /(max_value - min_value) # 转成numpy数组
+        print(normal_spectal)
+        plt.plot(normal_spectal)
+        plt.title(head_list[2])
+        plt.xlabel('Bands')
+        plt.ylabel('Relative spectral response')
+        plt.savefig(path.replace('.txt', '.png'))
+        plt.show()
